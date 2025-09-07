9ddd2b46057ac8e82b51a4fb9dbb16c6c0c84363
TepiTrade <comercialterraesol@gmail.com>
2025-09-07 14:09:28 -0300

    Corrige aplicativo.py sem duplicações

--- aplicativo.py
+++ aplicativo.py
@@ -1,20 +1,3 @@
-
-@app.route('/')
-def painel():
-    return render_template('index.html')
-
-@app.route('/bot/sinais')
-def sinais():
-    return render_template('sinais.html')
-
-@app.route('/bot/automatico')
-def automatico():
-    return render_template('automatico.html')
-
-@app.route('/bot/cripto')
-def cripto():
-    return render_template('cripto.html')
-
 @app.route('/')
 def painel():
     return render_template('index.html')
